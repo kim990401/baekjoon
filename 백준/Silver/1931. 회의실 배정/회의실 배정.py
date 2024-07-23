01edb2359f8a2db6fcaebfde2295e1b4b21d5697
kim990401 <xogus1p@gmail.com>
2024-07-23 18:24:32 +0900

    [Silver I] Title: 회의실 배정, Time: 4372 ms, Memory: 51900 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,13 @@
+N = int(input())
+time = []
+for i in range(N) :
+    time.append(list(map(int,input().split())))
+time.sort(key=lambda time: time[0])
+time.sort(key=lambda time: time[1])
+ans = time[0][1]
+result = 1
+for i in range(1,N) :
+    if time[i][0] >= ans :
+        ans = time[i][1]
+        result += 1
+print(result)
