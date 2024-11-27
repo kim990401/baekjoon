432e6b4a97a34e5b7af0a7eade38a65a7be19bc6
kim990401 <xogus1p@gmail.com>
2024-11-27 16:21:05 +0900

    [Gold III] Title: 파티, Time: 1028 ms, Memory: 34212 KB -BaekjoonHub

---
+++ 백준/Gold/1238. 파티/파티.py
@@ -0,0 +1,36 @@
+import sys, heapq
+INF = sys.maxsize
+N, M, X = map(int,input().split())
+route = [[] for _ in range(N+1)]
+
+for i in range(M) :
+    a, b, c = map(int, input().split())
+    route[a].append([b, c])    
+
+def dijkstra(start) :
+    distance = [INF] * (N+1)
+    q = []
+    distance[start] = 0
+    heapq.heappush(q, (0, start))
+    while q :
+        cur_distance, cur_node = heapq.heappop(q)
+        if cur_distance > distance[cur_node] :
+            continue
+        for next_node, next_distance in route[cur_node] :
+            new_distance = cur_distance + next_distance
+            if new_distance < distance[next_node] :
+                distance[next_node] = new_distance
+                heapq.heappush(q, (new_distance, next_node))
+    return distance
+
+ans = [0] * (N+1)
+for i in range(1, N+1) :
+    if i == X :
+        arr = dijkstra(i)
+        for j in range(1, N+1) :
+            ans[j] += arr[j]
+    else :
+        ans[i] += dijkstra(i)[X]
+
+print(max(ans))
+    
