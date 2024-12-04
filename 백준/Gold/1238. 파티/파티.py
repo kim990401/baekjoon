587c5a460bf62f27d9f9209fbf480600d5c1d30e
kim990401 <xogus1p@gmail.com>
2024-12-04 21:50:47 +0900

    [Gold III] Title: 파티, Time: 828 ms, Memory: 34212 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,36 +1,38 @@
 import sys, heapq
 INF = sys.maxsize
-N, M, X = map(int,input().split())
+input = sys.stdin.readline
+
+N, M, X = map(int, input().split())
 route = [[] for _ in range(N+1)]
 
-for i in range(M) :
+for _ in range(M):
     a, b, c = map(int, input().split())
-    route[a].append([b, c])    
+    route[a].append((b, c))
 
-def dijkstra(start) :
+def dijkstra(start):
     distance = [INF] * (N+1)
     q = []
     distance[start] = 0
     heapq.heappush(q, (0, start))
-    while q :
+    while q:
         cur_distance, cur_node = heapq.heappop(q)
-        if cur_distance > distance[cur_node] :
+        if cur_distance > distance[cur_node]:
             continue
-        for next_node, next_distance in route[cur_node] :
-            new_distance = cur_distance + next_distance
-            if new_distance < distance[next_node] :
+        for next_node, weight in route[cur_node]:
+            new_distance = cur_distance + weight
+            if new_distance < distance[next_node]:
                 distance[next_node] = new_distance
                 heapq.heappush(q, (new_distance, next_node))
     return distance
 
-ans = [0] * (N+1)
-for i in range(1, N+1) :
-    if i == X :
-        arr = dijkstra(i)
-        for j in range(1, N+1) :
-            ans[j] += arr[j]
-    else :
-        ans[i] += dijkstra(i)[X]
+# X에서 모든 노드로 가는 거리
+to_X = dijkstra(X)
 
-print(max(ans))
-    
+# 모든 노드에서 X로 가는 거리
+max_time = 0
+for i in range(1, N+1):
+    if i != X:
+        from_i = dijkstra(i)
+        max_time = max(max_time, from_i[X] + to_X[i])
+
+print(max_time)
